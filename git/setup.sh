69bb0ca37bdd81c296d4a4a600c97440528a5fff
Jason Green <Jason.Green@AverInformatics.Com>
2015-09-11 14:54:23 -0400

    Add git bash completion

---
+++ git/setup.sh
@@ -0,0 +1,3 @@
+source $DOTBIN/git/git-completion.bash.sh
+source $DOTBIN/git/git-flow-completion.sh
+source $DOTBIN/git/git.sh
--- profile.sh
+++ profile.sh
@@ -2,10 +2,19 @@
 [ -z "$PS1" ] && return
 
 export DOTBIN="${HOME}/.bin"
-
+source $DOTBIN/git/setup.sh
 source $DOTBIN/misc_functions/*.sh
-source $DOTBIN/misc_setup/*.sh
-source $DOTBIN/git/*.sh
+
+SETUP=$DOTBIN/misc_setup
+source $SETUP/aliases.sh
+source $SETUP/completion.sh
+source $SETUP/docker.sh
+source $SETUP/rouster.sh
+source $SETUP/history.sh
+source $SETUP/prompt.sh
+source $SETUP/virtualenv.sh
+
+
 
 SCRIPTS="${DOTBIN}/scripts"
 export PATH="${PATH}:$SCRIPTS"
@@ -22,5 +31,3 @@ date
 if [ -f ~/.localbin/local_profile ]; then
     source ~/.localbin/local_profile
 fi
-
-#test_network
