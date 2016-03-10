446c0fcc5e8cdbc26dc4f280a019aa2d69ae7640
Jason Green <Git@JasonGreen.Name>
2016-03-10 17:32:21 -0500

    Add RISE (Reveal-js Ipython Slideshow Extension) alias

--- setup/aliases.sh
+++ setup/aliases.sh
@@ -1,22 +1,10 @@
-# Misc.
-
-alias en='sudo bash -rcfile $DOTBIN/profile.sh'
-alias apropos='LESS=${LESS:--}S /usr/bin/apropos'
-alias rdme='less *[Rr][Ee][Aa][Dd]*[Mm][Ee]*'
-alias pf="clear;find . | grep -v '.git' | grep -v '.pyc$' | grep -v '__pycache__' | grep -v '~$' | grep -v '/#' | grep -v '/.#' | less"
-alias ddiff='clear;diff -C3 -rNX .gitignore'
-alias biggest="du -s ./* | sort -nr | awk '{print \$2}' | xargs du -sh"
-
 # Listings
 alias cls='clear;lls'
 alias clls='clear;llls'
 alias lsfuncs='declare -f'
 
 # MTR
-alias mtr=/usr/local/sbin/mtr
-
-# Apt
-alias get='sudo apt-get install'
+alias smtr='sudo /usr/local/sbin/mtr'
 
 # Silly
 alias armageddon='ps -auxww | grep ^USERNAME | sed -e "s/  */ /g" | cut -d " " -f 2 | xargs -n 4000 kill -9'
@@ -26,3 +14,12 @@ alias armageddon='ps -auxww | grep ^USERNAME | sed -e "s/  */ /g" | cut -d " " -
 alias del.pyc='find . -type f -name "*.pyc" -delete'
 alias del.tilde='find . -type f -name "*~" -delete'
 alias del.clean="del.pyc && del.tilde"
+
+# Misc.
+alias en='sudo bash -rcfile $DOTBIN/profile.sh'
+alias apropos='LESS=${LESS:--}S /usr/bin/apropos'
+alias rdme='less *[Rr][Ee][Aa][Dd]*[Mm][Ee]*'
+alias pf="clear;find . | grep -v '.git' | grep -v '.pyc$' | grep -v '__pycache__' | grep -v '~$' | grep -v '/#' | grep -v '/.#' | less"
+alias ddiff='clear;diff -C3 -rNX .gitignore'
+alias biggest="du -s ./* | sort -nr | awk '{print \$2}' | xargs du -sh"
+alias rise='docker run --rm -it -p 8888:8888 -v "$(pwd):/notebooks" gjigsaw/rise:latest'
