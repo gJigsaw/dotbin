fc4d27ef56fa7b3b02b4035e8f22852e320a9d84
gJigsaw <GitHub@JasonGreen.Name>
2019-06-11 16:18:03 -0400

    Update profile.sh to reflect recent commits.

--- profile.sh
+++ profile.sh
@@ -1,14 +1,18 @@
 # Bail if not running interactively.
 [ -z "$PS1" ] && return
 
+# Set Paths
 export DOTBIN="${HOME}/.bin"
-source $DOTBIN/git/setup.sh
-
-source $DOTBIN/tmux/setup.sh
 export DOCKER="${HOME}/docker"
-source $DOCKER/setup.sh
-
 MISC_FUNCS=$DOTBIN/misc_functions
+SETUP=$DOTBIN/setup
+SCRIPTS="${DOTBIN}/scripts"
+
+export PATH="${PATH}:$SCRIPTS"
+
+source $DOTBIN/git/setup.sh
+source $DOTBIN/tmux/setup.sh
+source $DOCKER/setup.sh
 
 source $MISC_FUNCS/backup
 source $MISC_FUNCS/browse
@@ -23,11 +27,7 @@ source $MISC_FUNCS/timestamp
 source $MISC_FUNCS/whack
 source $MISC_FUNCS/what
 
-
-SETUP=$DOTBIN/setup
-
 source $SETUP/aliases.sh
-
 source $SETUP/completion.sh
 source $SETUP/history.sh
 source $SETUP/rise.sh
@@ -36,8 +36,8 @@ source $SETUP/virtualenv.sh
 
 source ~/docker/now/now.sh
 
-SCRIPTS="${DOTBIN}/scripts"
-export PATH="${PATH}:$SCRIPTS"
+
+
 
 export LESS="-iQFXKRM --shift=5"
 export EDITOR="/usr/bin/emacs"
